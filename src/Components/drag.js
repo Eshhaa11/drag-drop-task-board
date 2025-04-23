8a817bbf11f6af0ee6ca5315e9e226760f36dbca
Esha Patel <Patellesha11@gmail.com>
2025-04-23 14:26:39 +0300

    imported react

--- src/Components/drag.js
+++ src/Components/drag.js
@@ -1,16 +1,5 @@
 import React, { useState } from "react";
 
-function DragButton () {
-    const [drag, setDrag] = useState(false);
-    const [position, setPosition] = useState({ x: 0, y: 0 });
 
-    const handleDragStart = (e) => {
-        setDrag(true);
-        e.dataTransfer.setData("text/plain", "");
-    };
 
-    const handleDragOver = (e) => {
-        if (dragged) {
-            const { clientX, clientY } = e;
-            setPosition({ x: clientX - 50, y: clientY - 50 });
-}
+    
